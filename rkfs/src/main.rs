c6d89c362d586092182176a1b17b408bcb6dced7
intrusions <xeloronkali@gmail.com>
2025-03-18 13:38:40 +0100

    clean():

--- rkfs/src/main.rs
+++ rkfs/src/main.rs
@@ -1,9 +1,7 @@
-// https://os.phil-opp.com/minimal-rust-kernel/
-// https://wiki.osdev.org/User:Zesterer/Bare_Bones
-
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -11,18 +9,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World from rkfs!";
-
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();    
 
     loop {}
 }
